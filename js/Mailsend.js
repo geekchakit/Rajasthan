177fd8d3f4177429619eb375784a30204b56dffc
Chakit Sharma <chakitsharma444@outlook.com>
2023-06-27 00:02:22 +0530

    Rajasthan done

---
+++ js/Mailsend.js
@@ -0,0 +1,79 @@
+  function SendEnquery()
+  {
+    $("#myModal").modal("show");
+  }
+
+  function PackageEnquiry(PackageName)
+  {
+    $("#myModal").modal("show");
+    localStorage.setItem("pkgname",PackageName)
+  }
+
+  function SendLead(){
+    var templateParams = {
+      from_name: $("#FullNameModal").val(),
+      email_id: $("#EmailModal").val(),
+      contact_no:$("#NumberModal").val(),
+      package_name:localStorage.getItem('pkgname'),
+      startdate:$("#DateModal").val(),
+      adult:$("#AdultModal").val(),
+      // group:$("#GroupPKG").val(),
+      message:$("#MessageModal").val(),
+      website:"Rajasthan"
+  };
+//    console.log(templateParams);
+  emailjs.send('service_67t7hhj', 'template_thv6rqb', templateParams,'nmhVg3RXzbcSkzs_u')
+      .then(function(response) {
+          swal("Good job!", "Your Response Has been submited!", "success");
+          $("#FullNameModal").val('');
+          $("#EmailModal").val('');
+          $("#NumberModal").val('');
+          $("#MessageModal").val('');
+          // $("#GroupPKG").val('');
+          $("#AdultModal").val('');
+          $("#DateModal").val('');
+          $("#myModal").modal("hide");
+          $("#MineBntModal").prop('disabled',false);
+          localStorage.setItem(null);
+      }, function(error) {
+          swal("Sorry!", "Its not your fault!", "error");
+          $("#MineBntModal").prop('disabled',false);
+      });
+  }
+
+  function SendLeadForm()
+  {
+    var templateParams = {
+      from_name: $("#FullNameForm").val(),
+      email_id: $("#EmailForm").val(),
+      contact_no:$("#NumberForm").val(),
+      package_name:"Sent From Form",
+      startdate:$("#DateForm").val(),
+      adult:$("#AdultForm").val(),
+      // group:$("#GroupPKG").val(),
+      message:$("#MessageForm").val(),
+      website:"Rajasthan"
+  };
+//    console.log(templateParams);
+  emailjs.send('service_67t7hhj', 'template_thv6rqb', templateParams,'nmhVg3RXzbcSkzs_u')
+      .then(function(response) {
+          swal("Good job!", "Your Response Has been submited!", "success");
+          $("#FullNameForm").val('');
+          $("#EmailForm").val('');
+          $("#NumberForm").val('');
+          $("#MessageForm").val('');
+          // $("#GroupPKG").val('');
+          $("#AdultForm").val('');
+          $("#DateForm").val('');
+          $("#MineBntForm").prop('disabled',false);
+          localStorage.setItem(null);
+      }, function(error) {
+          swal("Sorry!", "Its not your fault!", "error");
+          $("#MineBntForm").prop('disabled',false);
+      });
+  }
+
+  function CloseModal(){
+    $("#myModal").modal('hide');
+  }
+  
---
+++ js/app.whatsaap.js
@@ -0,0 +1,3 @@
+document.getElementById("ht-ctc-chat").addEventListener('click',function(){
+    window.open("https://api.whatsapp.com/send/?phone=918117887130&text=%22Hi+Traveler+%EF%BF%BD%0A%0AHope+you+are+doing+good.%0A%0AKindly+share+your+travel+plans++below+%3A+%EF%BF%BD%0A%0A1.+Number+of+Travelers+-+%0A2.+Places+in+your+Mind+-+%0A3.+Travel+Dates+-+%0A4.+Pickup+%26+Drop+-%0A5.+Duration+of+Trip+-%0A6.+Approx+Budget+per+person+-++%0A7.+Best+time+to+call+-%0A%0ALooking+forward+to+hear++from+you.%0A%0AThanks%0ATeam+DarshanRath&type=phone_number&app_absent=0")
+})
